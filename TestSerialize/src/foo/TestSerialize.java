ad4d504bccb62c2aabf933553cc0bb715316ee90
kevin wright <wrightkevin2000@gmail.com>
2012-07-21 11:23:26 +0100

    first cut

---
+++ TestSerialize/src/foo/TestSerialize.java
@@ -0,0 +1,14 @@
+package foo;
+
+public class TestSerialize {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this program should serialize something");
+
+	}
+
+}
